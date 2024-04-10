9e9d83ffece0f7a11f1d6a8f8250175ad60762f3
Deepak Kumar Sinha <94794077+sinhadeepak1115@users.noreply.github.com>
2024-04-11 00:04:26 +0530

    monorepo update

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,13 +1,33 @@
 import express from "express";
+import db from "@repo/db/client";
 
 const app = express();
 
-app.post("/webhook", (req, res) => {
+app.post("/hdfcwebhook", async (req, res) => {
   //TODO: Add zod validation here
   const paymentInformation = {
     token: req.body.token,
     userId: req.body.userId,
     amount: req.body.amount,
   };
+  await db.balance.update({
+    where: {
+      userId: paymentInformation.userId,
+    },
+    data: {
+      amount: {
+        increment: paymentInformation.amount,
+      },
+    },
+  });
   //Update the user's balance
+  await db.onRampTransaction.update({
+    where: {
+      token: paymentInformation.token,
+    },
+    data: {
+      status: "Success",
+    },
+  });
+  res.json({ message: "Success" });
 });
